87ef9bdfcacd3eed242478e145bcd9b57d4cecb5
Fatih Emre Aksoy <116756659+enigma0x1@users.noreply.github.com>
2025-01-02 03:37:33 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    publicDir: false // public klasörünü devre dışı bırak
+  server: {
+    host: true,
+    port: process.env.PORT || 3000, // Render için port ayarı
+    strictPort: true,
+  },
+  preview: {
+    host: true,
+    port: process.env.PORT || 3000, // Render için port ayarı
+    strictPort: true,
   }
 })
